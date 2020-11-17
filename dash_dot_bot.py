2ffd8fd3f2c15a9aa0f4d7e7a67c6a76f1c7520d
Sean Dunn <spdunn@protonmail.com>
2020-11-17 14:13:01 -0500

    Add master loop for UI

--- dash_dot_bot.py
+++ dash_dot_bot.py
@@ -9,6 +9,7 @@ import time
 
 
 # TODO: write master loop for the interface
+# TODO: write function to perform the translation
 # TODO: log transcript of the translation session to a .txt file
 # TODO: use the 'time' module and an equation to print the answers in the correct tempo & rhythm
 
@@ -51,7 +52,21 @@ key = {
     '0': '- - - - -'
 }
 
-for x in key:
-    print(key[x])
-    time.sleep(1)
+# This loop is for testing
+# Delete soon
+# for x in key:
+#     print(key[x])
+#     time.sleep(1)
 
+# TODO: add a count to make intro statement only appear upon first opening app
+while True:
+    print("""Hi, I'm Dash Dot Bot.
+    I translate what you say into Morse code!
+    Type something in and I'll say it back to you in Morse code!""")
+    user_input = input("""To view the conversion chart, type 'view chart'.
+    Or, type 'Q' to Quit """)
+    if user_input.upper() == "VIEW CHART":
+        print(key)
+        # Do i need a 'continue' statement here?
+    if user_input.upper() == "Q":
+        break
