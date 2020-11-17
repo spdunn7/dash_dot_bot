f6c025e84c4837be0f1ac6ecefa73ab8e2b713b5
Sean Dunn <spdunn@protonmail.com>
2020-11-16 20:32:32 -0500

    Start dict

--- dash_dot_bot.py
+++ dash_dot_bot.py
@@ -6,5 +6,17 @@ Created: 2020-10-10
 Author: Sean P. Dunn
 """
 
+# TODO: write master loop for the interface
+# TODO: add dictionary w/ English - Morse translations
+# TODO: log transcript of the translation session to a .txt file
+# TODO: use the 'time' module and an equation to print the answers in the correct tempo & rhythm
 
-print('So it begins...')
+# I've added a space after each letter to add the space between letters when spelling a word
+key = {
+    'a': '. - ',
+    'b': '- . . . ',
+    'c': '- . - . ',
+    'd': '- . . '
+}
+print(key)
+print(key[1])
